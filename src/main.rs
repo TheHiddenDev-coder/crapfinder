d34a34761553ad54a927fce9134fbec4053b6f13
TheHiddenDev-coder <ericboboiciov@gmail.com>
2026-07-27 21:17:31 +0300

    First functional release

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,34 @@
 #![allow(unused, unused_mut)]
 
-use std::env;
+use std::{
+    env,
+    fs::File,
+    io::{self, BufRead, BufReader},
+    path::Path,
+    process,
+};
 
-fn main() {
-    let mut args: Vec<String> = env::args().collect();
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
 
-    for (i, arg) in args.iter().enumerate() {
-        println!("Arg {}: {}", i, arg)
+    if args.len() < 3 {
+        eprintln!("Usage:");
+        eprintln!("crapfinder <path> <\"text\">");
+        process::exit(1);
     }
+
+    let file_path = &args[1];
+    let pattern = &args[2];
+
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+
+    for (i, line) in reader.lines().enumerate() {
+        let line = line?;
+
+        if line.to_lowercase().contains(&pattern.to_lowercase()) {
+            println!("{}:{} <> {}", file_path, i + 1, line);
+        }
+    }
+    Ok(())
 }
