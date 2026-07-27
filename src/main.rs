3ef4bab1a819c3875102c83bd180aeffbe804740
TheHiddenDev-coder <ericboboiciov@gmail.com>
2026-07-27 14:01:18 +0300

    ADDED temporary argument collection functionality

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,11 @@
-fn main() {}
+#![allow(unused, unused_mut)]
+
+use std::env;
+
+fn main() {
+    let mut args: Vec<String> = env::args().collect();
+
+    for (i, arg) in args.iter().enumerate() {
+        println!("Arg {}: {}", i, arg)
+    }
+}
